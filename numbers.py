4795544eafe55071006f41f6e5f0ad8d28b54cb0
nurila26 <nurila260681@gmail.com>
2025-01-19 10:59:27 +0500

    lab1

---
+++ numbers.py
@@ -0,0 +1,18 @@
+
+
+a = int(1)   # a will be 1
+b = int(2.8) # b will be 2
+c = int("3") # c will be 3
+print(a,b,c)
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+print(x,y,z,w)
+
+i = str("s1") # i will be 's1'
+l= str(2)    # l will be '2'
+k = str(3.0)  # k will be '3.0'
+print(i,l,k)
+
